79e59777e0d5e514bdea39888537124059fab415
Kayiranga <donjesuskayiranga@gmail.com>
2026-03-19 10:18:19 +0200

    simple changes

--- don/connection.php
+++ don/connection.php
@@ -1,17 +1,12 @@
 <?php
-
 $servername = 'localhost';
-$username = 'kayiranga';
-$password = 'donca';
-$dbname = 'year1c';
+$username   = 'kayiranga';
+$password   = 'donca';
+$dbname     = 'year1c';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if(!$conn){
-    exit('Connection Failed: '.mysqli_connect_error());
+if ($conn->connect_errno) {
+    exit('Connection Failed: ' . $conn->connect_error);
 }
-
-echo 'Connected Successfully';
-//mysqli_close($conn);
-
 ?>
