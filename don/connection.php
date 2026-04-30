cfa87a9988b791e3d74e0682963fa7e87b356d43
Kayiranga <donjesuskayiranga@gmail.com>
2026-04-30 14:55:27 +0200

    connecting to the database

--- don/connection.php
+++ don/connection.php
@@ -3,12 +3,11 @@ $servername = 'localhost';
 $username   = 'kayiranga';
 $password   = 'donca';
 $dbname     = 'year1c';
-
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_errno) {
-    
-    exit('Connection Failed: ' . $conn->connect_error);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+try {
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn->set_charset('utf8mb4');
+} catch (mysqli_sql_exception $e) {
+    exit('Database connection failed. Please check your database name, username, and password.');
 }
 ?>
